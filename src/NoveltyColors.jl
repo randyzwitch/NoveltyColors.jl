aa7fd4c0106c2254e00fa7b292b305112db49991
randyzwitch <rzwitch@gmail.com>
2016-09-02 10:36:18 -0400

    Update for relative path
    Fixes #5

--- src/NoveltyColors.jl
+++ src/NoveltyColors.jl
@@ -4,10 +4,11 @@ export ColorDict
 import JSON: parsefile
 
 ColorDict = Dict{AbstractString,Any}()
-for file in readdir(Pkg.dir("NoveltyColors", "data"))
+for file in readdir(dirname(@__FILE__) * "/../data/")
     if file[end-4:end] == ".json"
         merge!(ColorDict, Dict{AbstractString,Any}(file[1:end-5] => (parsefile(Pkg.dir("NoveltyColors", "data", file)))))
     end
 end
 
 end # module
+
