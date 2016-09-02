23726bc1bd9c86ad104c8a60438e353e6dd2e974
randyzwitch <rzwitch@gmail.com>
2016-09-02 10:48:54 -0400

    Fix other dir reference

--- src/NoveltyColors.jl
+++ src/NoveltyColors.jl
@@ -3,10 +3,12 @@ module NoveltyColors
 export ColorDict
 import JSON: parsefile
 
+directory = dirname(@__FILE__) * "/../data/"
+
 ColorDict = Dict{AbstractString,Any}()
-for file in readdir(dirname(@__FILE__) * "/../data/")
+for file in readdir(directory)
     if file[end-4:end] == ".json"
-        merge!(ColorDict, Dict{AbstractString,Any}(file[1:end-5] => (parsefile(Pkg.dir("NoveltyColors", "data", file)))))
+        merge!(ColorDict, Dict{AbstractString,Any}(file[1:end-5] => (parsefile(directory * file))))
     end
 end
 
