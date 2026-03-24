b6979e22290041179978572fdc6776ba2ccb6771
Randy Zwitch <rzwitch@gmail.com>
2026-03-24 16:39:55 -0400

    Update CI to GitHub (#13)
    * Update CI to GitHub
    
    * Remove old badge
    
    * Update old specifications
    
    * Update TagBot
    
    * Fix concatenation

--- src/NoveltyColors.jl
+++ src/NoveltyColors.jl
@@ -3,12 +3,12 @@ module NoveltyColors
 export ColorDict
 import JSON: parsefile
 
-directory = dirname(@__FILE__) * "/../data/"
+directory = joinpath(@__DIR__, "..", "data")
 
 ColorDict = Dict{AbstractString,Any}()
 for file in readdir(directory)
     if file[end-4:end] == ".json"
-        merge!(ColorDict, Dict{AbstractString,Any}(file[1:end-5] => (parsefile(directory * file))))
+        merge!(ColorDict, Dict{AbstractString,Any}(file[1:end-5] => (parsefile(joinpath(directory, file)))))
     end
 end
 
