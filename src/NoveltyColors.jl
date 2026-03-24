984ff027db7d30e71694f98f87c34cccfbce55eb
Randy Zwitch <rzwitch@gmail.com>
2026-03-24 16:07:25 -0400

    Fix concatenation

--- src/NoveltyColors.jl
+++ src/NoveltyColors.jl
@@ -8,7 +8,7 @@ directory = joinpath(@__DIR__, "..", "data")
 ColorDict = Dict{AbstractString,Any}()
 for file in readdir(directory)
     if file[end-4:end] == ".json"
-        merge!(ColorDict, Dict{AbstractString,Any}(file[1:end-5] => (parsefile(directory * file))))
+        merge!(ColorDict, Dict{AbstractString,Any}(file[1:end-5] => (parsefile(joinpath(directory, file)))))
     end
 end
 
